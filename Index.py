fc9d90a37f49b0fbedd939e561042bdf44eef000
C-Ernesto <christopher.ernesto.02@gmail.com>
2024-05-05 20:34:00 -0700

    Updated Index.py to make it a class so vectorizer can be accessed from other classes

--- Index.py
+++ Index.py
@@ -2,86 +2,102 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from pymongo import MongoClient
 from collections import OrderedDict
 
+class Index:
 
-def connectDataBase():
-    # Create a database connection object using pymongo
+    def __init__(self, docsDict):
+        self.docsDict = docsDict
+        self.countVectorizer = CountVectorizer()
+        self.tfidfVectorizer = TfidfVectorizer()
+        self.inverted_index = {}
 
-    DB_NAME = "project"
-    DB_HOST = "localhost"
-    DB_PORT = 27017
+    def getCountVectorizer(self):
+        return self.countVectorizer
 
-    try:
+    def getTfidfVectorizer(self):
+        return self.tfidfVectorizer
 
-        client = MongoClient(host=DB_HOST, port=DB_PORT)
-        db = client[DB_NAME]
+    def getInvertedIndex(self):
+        return self.inverted_index
 
-        return db
+    def connectDataBase(self):
+        # Create a database connection object using pymongo
 
-    except:
-        print("Database not connected successfully")
+        DB_NAME = "project"
+        DB_HOST = "localhost"
+        DB_PORT = 27017
 
+        try:
 
-def create_inverted_index(docsDict):
+            client = MongoClient(host=DB_HOST, port=DB_PORT)
+            db = client[DB_NAME]
 
-    # convert docs to ordered dict
-    docs = OrderedDict(docsDict)
+            return db
 
-    # convert documents to list
-    corpus = list(docs.values())
-
-    # Create a CountVectorizer to get word counts
-    countVectorizer = CountVectorizer()
-    count_matrix = countVectorizer.fit_transform(corpus)
-    count_matrix_np = count_matrix.toarray()
-
-    # Create TF-IDF vectorizer
-    tfidfVectorizer = TfidfVectorizer()
-    tfidf_matrix = tfidfVectorizer.fit_transform(corpus)
-    tfidf_matrix_np = tfidf_matrix.toarray()
-
-    # Get terms
-    feature_names = tfidfVectorizer.get_feature_names_out()
-
-    inverted_index = {}
-
-    # Iterate over terms and documents to build inverted index
-    # tfidf_matrix is a 2d array where row = doc_id and col = term
-    for i, term in enumerate(feature_names):
-        inverted_index[term] = []
-        for doc_id, text in docs.items():
-            tfidf = tfidf_matrix_np[doc_id][i]
-            count = count_matrix_np[doc_id][i].item()
-            # only consider non-zero terms
-            if tfidf > 0:
-                inverted_index[term].append({
-                    "doc_id": doc_id,
-                    "count": count,
-                    "tfidf": tfidf
-                })
-
-    # print(inverted_index)
-
-    # convert dictionary to a list
-    inverted_index_document = []
-    for term, docs in inverted_index.items():
-        inverted_index_document.append({
-            "term": term,
-            "documents": docs
-        })
-
-    # print(inverted_index_document)
-
-    return inverted_index_document
+        except:
+            print("Database not connected successfully")
 
 
-def start_indexing(docs):
-    db = connectDataBase()
-    index = db.index
+    def create_inverted_index(self):
 
-    inverted_index = create_inverted_index(docs)
+        # convert docs to ordered dict
+        docs = OrderedDict(self.docsDict)
 
-    # store inverted index
-    index.insert_many(inverted_index)
+        # convert documents to list
+        corpus = list(docs.values())
+
+        # Create a CountVectorizer to get word counts
+        # countVectorizer = CountVectorizer()
+        count_matrix = self.countVectorizer.fit_transform(corpus)
+        count_matrix_np = count_matrix.toarray()
+
+        # Create TF-IDF vectorizer
+        # tfidfVectorizer = TfidfVectorizer()
+        tfidf_matrix = self.tfidfVectorizer.fit_transform(corpus)
+        tfidf_matrix_np = tfidf_matrix.toarray()
+
+        # Get terms
+        feature_names = self.tfidfVectorizer.get_feature_names_out()
+
+        # inverted_index = {}
+
+        # Iterate over terms and documents to build inverted index
+        # tfidf_matrix is a 2d array where row = doc_id and col = term
+        for i, term in enumerate(feature_names):
+            self.inverted_index[term] = []
+            for doc_id, text in docs.items():
+                tfidf = tfidf_matrix_np[doc_id][i]
+                count = count_matrix_np[doc_id][i].item()
+                # only consider non-zero terms
+                if tfidf > 0:
+                    self.inverted_index[term].append({
+                        "doc_id": doc_id,
+                        "count": count,
+                        "tfidf": tfidf
+                    })
+
+        # print(inverted_index)
+
+        # convert dictionary to a list
+        inverted_index_document = []
+        for term, docs in self.inverted_index.items():
+            inverted_index_document.append({
+                "term": term,
+                "documents": docs
+            })
+
+        # print(inverted_index_document)
+
+        return inverted_index_document
+
+
+    def start_indexing(self):
+        db = self.connectDataBase()
+        index = db.index
+
+        inverted_index = self.create_inverted_index()
+
+        # store inverted index
+        index.insert_many(inverted_index)
 
 
 if __name__ == '__main__':
@@ -92,4 +108,7 @@ if __name__ == '__main__':
         2: "apple orange"
     }
 
-    start_indexing(documents)
+    index = Index(documents)
+    index.start_indexing()
+
+    #start_indexing(documents)
